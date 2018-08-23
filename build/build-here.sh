96fd01ba126d7e5a2a636bd5aae540508927c955
Stephen Marquard <stephen.marquard@uct.ac.za>
2018-08-23 22:00:01 +0200

    AUTO: build backup - 2018-08-23 22:00:01

--- build/build-here.sh
+++ build/build-here.sh
@@ -8,6 +8,6 @@ source /usr/local/src/vula-12.x/profile.sh
 echo Building folder in place
 
 # Maven2 build commad
-mvn $1 -Dmaven.tomcat.home=$DEST -Dmaven.test.skip=true -Dmaven.repo.local=$LOCALMAVENREPO/repository/ clean install sakai:deploy 
-#mvn $1 -Dmaven.tomcat.home=$DEST -Dmaven.repo.local=$LOCALMAVENREPO/repository/ clean install sakai:deploy 
+#mvn $1 -Dmaven.tomcat.home=$DEST -Dmaven.test.skip=true -Dmaven.repo.local=$LOCALMAVENREPO/repository/ clean install sakai:deploy 
+mvn $1 -Dmaven.tomcat.home=$DEST -Dmaven.repo.local=$LOCALMAVENREPO/repository/ clean install sakai:deploy 
 
