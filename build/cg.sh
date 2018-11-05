f1ec3253db0f5dee29c116bb5b25aca79db04faf
Stephen Marquard <stephen.marquard@uct.ac.za>
2018-11-05 22:00:05 +0200

    AUTO: build backup - 2018-11-05 22:00:04

---
+++ build/cg.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+curl --verbose --user loadtest0099:abc99x "https://devslscle001.uct.ac.za/direct/announcement/user.json?__auth=basic&d=365&n=10"
+#curl --verbose --user loadtest0099:abc99x "https://devslscle001.uct.ac.za/direct/user/current.json?__auth=basic"
+
